c1b385d2aedd724bd7ad5dc1e8df74f473244e93
Ruslanux <zhubanov1090@gmail.com>
2025-12-18 13:46:12 +0500

    Fix rubocop style issues in migrations

--- db/migrate/20251218000003_create_solid_cable_messages.rb
+++ db/migrate/20251218000003_create_solid_cable_messages.rb
@@ -8,9 +8,9 @@ class CreateSolidCableMessages < ActiveRecord::Migration[7.1]
       t.datetime :created_at, null: false
       t.bigint :channel_hash, null: false
 
-      t.index [:channel], name: "index_solid_cable_messages_on_channel"
-      t.index [:channel_hash], name: "index_solid_cable_messages_on_channel_hash"
-      t.index [:created_at], name: "index_solid_cable_messages_on_created_at"
+      t.index [ :channel ], name: "index_solid_cable_messages_on_channel"
+      t.index [ :channel_hash ], name: "index_solid_cable_messages_on_channel_hash"
+      t.index [ :created_at ], name: "index_solid_cable_messages_on_created_at"
     end
   end
 end
